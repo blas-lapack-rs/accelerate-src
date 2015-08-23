4d7c7edad19dc10c44331957e3ea7783e9d39ed6
Andrew Straw <strawman@astraw.com>
2015-08-23 16:09:11 +0200

    initial implementation

---
+++ README.md
@@ -0,0 +1,21 @@
+# Accelerate Provider
+
+The package provides [BLAS][1] and [LAPACK][2] to [Rust][3] code using Apple's
+[Accelerate][4] framework. The package simply links the Accelerate framework
+that Apple ships with Mac OS X.
+
+## Where are all the FFI definitions?
+
+This package only links to an implementation of BLAS and LAPACK. Bindings are
+available in [blas-sys][5] and [lapack-sys][6], and wrappers are available in
+[blas][7] and [lapack][8].
+
+[1]: https://en.wikipedia.org/wiki/Basic_Linear_Algebra_Subprograms
+[2]: https://en.wikipedia.org/wiki/LAPACK
+[3]: https://www.rust-lang.org/
+[4]: https://developer.apple.com/library/mac/documentation/Accelerate/Reference/AccelerateFWRef/
+
+[5]: https://github.com/stainless-steel/blas-sys
+[6]: https://github.com/stainless-steel/lapack-sys
+[7]: https://github.com/stainless-steel/blas
+[8]: https://github.com/stainless-steel/lapack
---
+++ src/lib.rs
@@ -0,0 +1,38 @@
+#![allow(non_camel_case_types)]
+
+#[cfg(test)]
+extern crate libc;
+
+#[cfg(test)]
+mod tests {
+    use libc::{c_int, c_float};
+
+    // FIXME: I'm not sure if these next two lines are correct.
+    type vDSP_Stride = c_int;
+    type vDSP_Length = c_int;
+
+    extern "C" {
+        // Multiply vector __vDSP_A by scalar __vDSP_B and place result in __vDSP_C.
+        pub fn vDSP_vsmul(__vDSP_A: *const c_float, __vDSP_IA: vDSP_Stride, __vDSP_B: *const c_float, __vDSP_C: *mut c_float, __vDSP_IC: vDSP_Stride, __vDSP_N: vDSP_Length );
+    }
+
+    fn approx_eq(x: f32, y:f32) -> bool {
+        let eps = 1e-10;
+        (x - y).abs() < (eps * y).abs()
+    }
+
+    #[test]
+    fn check_linking() {
+        let input : [f32; 3] = [1.0,2.0,3.0];
+        let mut output : [f32; 3] = [0.0, 0.0, 0.0];
+        let factor: f32 = 5.0;
+        // multiply each element in [1,2,3] by 5
+        unsafe {
+            vDSP_vsmul(input.as_ptr(), 1, &factor, output.as_mut_ptr(), 1, 3);
+        }
+        assert!(approx_eq(output[0], 5.0));
+        assert!(approx_eq(output[1], 10.0));
+        assert!(approx_eq(output[2], 15.0));
+    }
+
+}
