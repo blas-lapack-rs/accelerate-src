bab7d00ac8b480717476d4ab9b8377a9fec94f9b
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-11-17 15:36:51 +0100

    Simplify the test

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,35 +1,21 @@
-#![allow(non_camel_case_types)]
-
 extern crate accelerate_src;
 extern crate libc;
 
-use libc::{c_float, ptrdiff_t, size_t};
+use libc::c_float;
 
-extern "C" {
-    fn vDSP_vsmul(
-        vDSP_input1: *const c_float,
-        vDSP_stride1: ptrdiff_t,
-        vDSP_input2: *const c_float,
-        vDSP_result: *mut c_float,
-        vDSP_strideResult: ptrdiff_t,
-        vDSP_size: size_t,
-    );
+extern {
+    pub fn srotg_(a: *mut c_float, b: *mut c_float, c: *mut c_float, s: *mut c_float);
 }
 
 #[test]
 fn link() {
-    let input = [1.0, 2.0, 3.0];
-    let factor = 5.0;
-    let mut output = [0.0, 0.0, 0.0];
     unsafe {
-        vDSP_vsmul(input.as_ptr(), 1, &factor, output.as_mut_ptr(), 1, 3);
+        let mut a: f32 = 0.0;
+        let mut b: f32 = 0.0;
+        let mut c: f32 = 42.0;
+        let mut d: f32 = 42.0;
+        srotg_(&mut a as *mut _, &mut b as *mut _, &mut c as *mut _, &mut d as *mut _);
+        assert!(c == 1.0);
+        assert!(d == 0.0);
     }
-    assert!(close(output[0], 5.0));
-    assert!(close(output[1], 10.0));
-    assert!(close(output[2], 15.0));
-}
-
-fn close(x: c_float, y: c_float) -> bool {
-    const EPS: c_float = 1e-10;
-    (x - y).abs() < (EPS * y).abs()
 }
