210506503a54b0099be91cab060f731b3661c850
Ivan Ukhov <ivan.ukhov@gmail.com>
2020-12-05 08:14:35 +0100

    Update the source

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 //! Source of [BLAS] and [LAPACK] via the [Accelerate] framework.
 //!
-//! The usage of the package is explained [here][usage].
+//! ## [Architecture]
 //!
 //! [accelerate]: https://developer.apple.com/documentation/accelerate
+//! [architecture]: https://blas-lapack-rs.github.io/architecture
 //! [blas]: https://en.wikipedia.org/wiki/BLAS
 //! [lapack]: https://en.wikipedia.org/wiki/LAPACK
-//! [usage]: https://blas-lapack-rs.github.io/usage
 
 #![no_std]
